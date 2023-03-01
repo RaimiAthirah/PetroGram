229e42be478044a82d8a2bba105bed74c5029173
Raimi Athirah <raimi_21001054@utp.edu.my>
2023-03-01 16:35:17 +0800

    try

--- src/Channel.java
+++ src/Channel.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Channel {
+public class Channel {
 }
--- src/ChannelKopi.java
+++ src/ChannelKopi.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ChannelKopi {
+import javax.swing.*;
+
+public class ChannelKopi extends JFrame {
+    private JPanel mainChannel;
+    private JTextField textField1;
+    private JLabel lblApp;
+
+    public ChannelKopi () {
+        setContentPane(mainChannel);
+        setTitle("Channel");
+        setSize(450,300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        ChannelKopi myFrame = new ChannelKopi();
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
